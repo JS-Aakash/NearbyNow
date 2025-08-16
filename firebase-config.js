0d93223eb85063e374587c175d6ba0cbd62ccd7a
JS-Aakash <jsaakash22@gmail.com>
2025-08-16 18:08:44 +0530

    Storage added

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js"; // ✅ use full version
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAusCQM4vEgjV3OGpyZTN7iBlEf3AxbaOY",
-    authDomain: "nearbynow07.firebaseapp.com",
-    projectId: "nearbynow07",
-    storageBucket: "nearbynow07.firebasestorage.app",
-    messagingSenderId: "384534035331",
-    appId: "1:384534035331:web:df33109271d972c16e94b6"
+    apiKey: "AIzaSyCzch-53vyEw_wTkopkBfmnYT-xQL4mG-A",
+    authDomain: "nearbynow-in.firebaseapp.com",
+    databaseURL: "https://nearbynow-in-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nearbynow-in",
+    storageBucket: "nearbynow-in.firebasestorage.app",
+    messagingSenderId: "43936475211",
+    appId: "1:43936475211:web:14642efc4b732dca3645ce"
 };
 
 const app = initializeApp(firebaseConfig);
